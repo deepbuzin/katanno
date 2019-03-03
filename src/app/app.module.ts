f2b833c946afb0c8c346320abd6fd2cb0df5f6a0
Dimbas <redisotschek@gmail.com>
2019-03-04 00:32:01 +0300

    add main screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms'; // <-- here
-import { RoundProgressModule } from 'angular-svg-round-progressbar'; // <-- here
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
+import { RouterModule, Routes} from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { ViewerComponent } from './components/viewer/viewer.component'; // <-- here
+
+const appRoutes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'editor/:id',      component: EditorComponent },
+  {
+    path: 'viewer',
+    component: ViewerComponent
+  },
+  { path: '',
+    redirectTo: '/main',
+    pathMatch: 'full'
+  },
+  { path: '**',
+    redirectTo: '/main',
+    pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    EditorComponent,
+    ViewerComponent
   ],
   imports: [
-    BrowserModule, 
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserAnimationsModule,
+    BrowserModule,
     FormsModule, // <-- here
     RoundProgressModule // <-- and here
   ],

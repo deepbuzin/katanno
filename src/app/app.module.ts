12a40351b7fec8c4eb1158de66ccabe41f21261c
Dimbas <redisotschek@gmail.com>
2019-03-04 23:56:25 +0300

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FilesComponent } from './files/files.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'editor/:id', component: EditorComponent },
+  { path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +29,11 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]

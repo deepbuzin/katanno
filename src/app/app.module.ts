8ea478d36215b9824bbfafb5c64a1c87c7ac38a7
Andrey Buzin <anbuzin@pm.me>
2019-03-05 02:05:57 +0300

    tweak indentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule, Routes } from '@angular/router';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { FilesComponent } from './files/files.component';
-import { EditorComponent } from './components/editor/editor.component';
-import { HomeComponent } from './components/home/home.component';
+import {AppComponent} from './app.component';
+import {FilesComponent} from './files/files.component';
+import {EditorComponent} from './components/editor/editor.component';
+import {HomeComponent} from './components/home/home.component';
+import {ViewerComponent} from './components/viewer/viewer.component';
+import {TableComponent} from './components/table/table.component';
+import {PreviewComponent} from './components/preview/preview.component';
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'editor/:id', component: EditorComponent },
-  { path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  { path: '**', redirectTo: '/home' }
+    {path: 'home', component: HomeComponent},
+    {path: 'editor/:id', component: EditorComponent},
+    {path: 'viewer', component: ViewerComponent},
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FilesComponent,
-    EditorComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FilesComponent,
+        EditorComponent,
+        HomeComponent,
+        ViewerComponent,
+        TableComponent,
+        PreviewComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(
+            appRoutes,
+        )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -1,48 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { readdir, stat } from 'fs';
-import { resolve } from 'path';
-import { shell } from 'electron';
+import {Component, OnInit} from '@angular/core';
+import {readdir, stat} from 'fs';
+import {resolve} from 'path';
+import {shell} from 'electron';
 
 @Component({
-  selector: 'app-files',
-  templateUrl: './files.component.html',
-  styleUrls: ['./files.component.scss']
+    selector: 'app-files',
+    templateUrl: './files.component.html',
+    styleUrls: ['./files.component.scss']
 })
 export class FilesComponent implements OnInit {
 
-  private currentPath: string = process.cwd();
-  private entries: Array<string>;
+    private currentPath: string = process.cwd();
+    private entries: Array<string>;
 
-  constructor() {
-    this.updateEntries();
-  }
-  ngOnInit() {}
-  private updateEntries() {
-    readdir('C:\\', (err: Error, files: [string]) => {
-      if (err) {
-        console.error(err);
-      }
-      this.entries = ['../'].concat(files);
-    });
-  }
-  private changeDir(newDir: string) {
-    const targetPath = resolve(this.currentPath, newDir);
-    stat(targetPath, (err, stats) => {
-      if (err) {
-        console.error(err);
-      }
-      if (stats.isFile()) {
-        this.openFile(targetPath);
-      } else if (stats.isDirectory()) {
-        this.currentPath = targetPath;
+    constructor() {
         this.updateEntries();
-      } else {
-        console.error(new Error(`Unknown file system object: ${targetPath}`));
-      }
-    });
-  }
-  private openFile(path: string) {
-    shell.openItem(path);
-  }
+    }
+
+    ngOnInit() {
+    }
+
+    private updateEntries() {
+        readdir('C:\\', (err: Error, files: [string]) => {
+            if (err) {
+                console.error(err);
+            }
+            this.entries = ['../'].concat(files);
+        });
+    }
+
+    private changeDir(newDir: string) {
+        const targetPath = resolve(this.currentPath, newDir);
+        stat(targetPath, (err, stats) => {
+            if (err) {
+                console.error(err);
+            }
+            if (stats.isFile()) {
+                this.openFile(targetPath);
+            } else if (stats.isDirectory()) {
+                this.currentPath = targetPath;
+                this.updateEntries();
+            } else {
+                console.error(new Error(`Unknown file system object: ${targetPath}`));
+            }
+        });
+    }
+
+    private openFile(path: string) {
+        shell.openItem(path);
+    }
 
 }

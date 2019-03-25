4a4b9b3a58f2eab33d59f7f68519662b3f70fd93
Dimbas <redisotschek@gmail.com>
2019-03-26 00:22:43 +0300

    add splash loader

--- src/entry.live.js
+++ src/entry.live.js
@@ -6,34 +6,55 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
-  setTimeout(() => {
-    // Create the browser window.
-    win = new BrowserWindow({width: 1280, height: 720, webPreferences: {
-            webSecurity: false
-        }})
+function createWindow() {
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-      pathname: 'localhost:4200',
-      protocol: 'http:',
-      slashes: true
-    }))
-
-    // Open the DevTools when in dev mode.
-    if(process.env.NODE_ENV=='development') {
-      win.webContents.openDevTools()
-      require('devtron').install()
-    }
-
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
-      win = null
+    loader = new BrowserWindow({
+        titleBarStyle: 'hidden',
+        frame: false,
+        width: 600,
+        height: 400,
+        alwaysOnTop: true,
     })
-  }, 12000)
+
+    loader.loadURL(`file://${__dirname}/splash.html`);
+
+    setTimeout(() => {
+        // Create the browser window.
+        win = new BrowserWindow({
+            width: 1280,
+            height: 720,
+            show: false,
+            webPreferences: {
+                webSecurity: false
+            }
+        });
+
+        // and load the index.html of the app.
+        win.loadURL(url.format({
+            pathname: 'localhost:4200',
+            protocol: 'http:',
+            slashes: true
+        }));
+
+        // Open the DevTools when in dev mode.
+        if (process.env.NODE_ENV === 'development') {
+            win.webContents.openDevTools();
+            require('devtron').install()
+        }
+
+        win.once('ready-to-show', () => {
+            loader.destroy();
+            win.show()
+        });
+
+        // Emitted when the window is closed.
+        win.on('closed', () => {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            win = null
+        })
+    }, 12000)
 }
 
 // This method will be called when Electron has finished
@@ -43,19 +64,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
